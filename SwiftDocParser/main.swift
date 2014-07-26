364a76c157c059a44f3de3484a74de712c79965c
tokorom <tokorom@gmail.com>
2014-07-26 22:18:33 +0900

    First commit

---
+++ SwiftDocParser/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDocParser
+//
+//  Created by ytokoro on 7/26/14.
+//  Copyright (c) 2014 tokoro. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
