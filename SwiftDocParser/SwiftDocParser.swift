ee3a6857d16409b76f1e1cdea2946f54a365aa80
tokorom <tokorom@gmail.com>
2014-07-27 15:47:41 +0900

    Implement first version

---
+++ SwiftDocParser/SwiftDocParser.swift
@@ -0,0 +1,64 @@
+//
+//  SwiftDocParser.swift
+//
+//  Created by ToKoRo on 2014-07-27.
+//
+
+import Foundation
+
+class SwiftDocParser {
+
+    private var document: HTMLDocument?
+
+    init(path: NSString) {
+        var error: NSError?
+        let htmlString = NSString.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: &error)
+        self.document = HTMLDocument(string: htmlString)
+    }
+
+    func functions() -> [String] {
+        let matches = document?.nodesMatchingSelector(".Swift .item .otp-indent")
+        var results = [String]()
+        if let elements = matches as? [AnyObject] {
+            for element in elements {
+                if let textContent = element.textContent {
+                    if !textContent.isEmpty {
+                        results += textContent
+                    }
+                }
+            }
+        }
+        return results
+    }
+
+    func types() -> [String] {
+        let matches = document?.nodesMatchingSelector("span.n a")
+        var results = [String]()
+        if let elements = matches as? [AnyObject] {
+            for element in elements {
+                if let textContent = element.textContent {
+                    if !textContent.isEmpty {
+                        results += textContent
+                    }
+                }
+            }
+        }
+        return results
+    }
+
+    func enumValues() -> [String] {
+        let matches = document?.nodesMatchingSelector(".constant .Swift .code-voice")
+        var results = [String]()
+        if let elements = matches as? [AnyObject] {
+            for element in elements {
+                if let textContent = element.textContent {
+                    if !textContent.isEmpty {
+                        results += textContent
+                    }
+                }
+            }
+        }
+        return results
+    }
+    
+}
--- SwiftDocParser/main.swift
+++ SwiftDocParser/main.swift
@@ -1,12 +1,46 @@
 //
 //  main.swift
-//  SwiftDocParser
 //
-//  Created by ytokoro on 7/26/14.
-//  Copyright (c) 2014 tokoro. All rights reserved.
+//  Created by ToKoRo on 2014-07-27.
 //
 
 import Foundation
 
-println("Hello, World!")
+let docsetPath = "/Users/ytokoro/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleiOS8.0.iOSLibrary.docset"
+let documentationDirectory = "Contents/Resources/Documents/documentation"
+let documentationPath = docsetPath.stringByAppendingPathComponent(documentationDirectory)
 
+let fileManager = NSFileManager()
+var sets = NSMutableSet()
+
+let enumerator = fileManager.enumeratorAtPath(documentationPath)
+var filePath: String? = enumerator.nextObject() as? NSString
+while nil != filePath {
+    if filePath!.hasSuffix("index.html") {
+        let path = documentationPath.stringByAppendingPathComponent(filePath!)
+        let docParser = SwiftDocParser(path: path)
+
+        let types = docParser.types()
+        for keyword in types {
+            if !sets.containsObject(keyword) {
+                sets.addObject(keyword)
+                println(keyword);
+            }
+        }
+        let enumValues = docParser.enumValues()
+        for keyword in enumValues {
+            if !sets.containsObject(keyword) {
+                sets.addObject(keyword)
+                println(keyword);
+            }
+        }
+        let functions = docParser.functions()
+        for keyword in functions {
+            if !sets.containsObject(keyword) {
+                sets.addObject(keyword)
+                println(keyword);
+            }
+        }
+    }
+    filePath = enumerator.nextObject() as? NSString
+}
